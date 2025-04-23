de50dbd2b29ee547ad617f29000ddb5b075688ab
Marcos O.Lima <98562810+mfghjr@users.noreply.github.com>
2025-04-23 16:55:03 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,16 @@
+let peso = 147;
+let altura = 1.84;
+ 
+let alturax = altura * altura;
+const imc = peso / alturax ;
+
+console.log(`o imc do luan é ${imc}`);
+
+var a,b,c,x,x2 ;
+a = 2;
+b = 1;
+c = 1;
+x = (-b - 4*(a+c)) / (2 * a);
+console.log(x);
+x2 = (-b + 4*(a+c)) / (2 * a);
+console.log(x2);
